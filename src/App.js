6373b6ecb67c6b99c4ccbf057cd37921b8811093
Ahana Vishwakarma <aha01na@gmail.com>
2023-03-07 23:56:29 +0530

    Add Google Authentication

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import Dashboard from './Dashboard';
 
 class App extends Component {
     render() {
@@ -11,6 +12,7 @@ class App extends Component {
                     <Routes>
                         <Route exact path='/' element={<Home />}></Route>
                         <Route exact path='/login' element={<Login />}></Route>
+                        <Route exact path='/dashboard' element={<Dashboard />}></Route>
                     </Routes>
                 </div>
             </Router>
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { Redirect } from 'react-router';
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
 
 function Login() {
     return (
-        <h1>cfff</h1>
+        <div className="form">
+            <div className="form-wrapper">
+                <form action="#">
+                    <h1>Sign in</h1>
+                    <div className="social">
+                        <GoogleOAuthProvider clientId='195165307652-m8iq8lcsi1obfn4c6nt77cme3ah2a1ap.apps.googleusercontent.com'>
+                            <GoogleLogin
+                                onSuccess={credentialResponse => {
+                                    const details = jwt_decode(credentialResponse.credential);
+                                    console.log(credentialResponse);
+
+                                }}
+                                onError={() => {
+                                    console.log('Login Failed');
+                                }}
+                            />
+                        </GoogleOAuthProvider>
+                    </div>
+                    {/* <span>Already have an account?</span>
+                    <input type="email" placeholder="Enter your email"></input>
+                    <input type="password" placeholder="password"></input>
+                    <a href="#">Forgot your password?</a>
+                    <button>Sign in</button> */}
+                </form>
+            </div>
+        </div>
     );
 }
 
