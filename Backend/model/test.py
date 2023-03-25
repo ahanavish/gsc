0cfa369c44c521ab6c893cff50a311eb036a919c
Aryan Deshpande <deshpandearyan1@gmail.com>
2023-03-25 22:38:55 +0530

    [Backend](file that mimics the main.py)

--- Backend/model/test.py
+++ Backend/model/test.py
@@ -7,6 +7,7 @@ input_data_string = sys.argv[1]
 
 # Parse the JSON data into a Python object
 input_data = json.loads(input_data_string)
+# pre processing
 
 with open('./output.json', 'w') as f:
     json.dump(input_data, f)
