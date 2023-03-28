531725a77b5b8e8a53b0d3559a31ec3160cc1eaf
Aryan Deshpande <deshpandearyan1@gmail.com>
2023-03-29 00:03:09 +0530

    [Backend](Added try catch block to fix timeseries.js error)

--- Backend/controller/TimeSeries.js
+++ Backend/controller/TimeSeries.js
@@ -6,15 +6,25 @@ module.exports = async function timeseries(uid){
     // return day and engy
     const snap = await getDoc(doc(db, 'users', `${uid}`));
     const data = snap.data();
-    var check = data.energy;
-    console.log(data);
+    var check;
+
+    
+    try{
+        check = data.energy;
+        if(check == undefined){
+            return false;
+        }
+
+    }catch(err){
+        return false
+    }
+    
+    console.log(data.energy);
     if(check != undefined){
         const energy = data.energy;
         const day = energy.day;
         const engy = energy.engy;
         return {day, engy};
-    }else{
-        return false
     }
 
 }
